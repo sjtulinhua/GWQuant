986cbe1a8be9472215b38b706a282471a81463e7
sjtulinhua <sjtulinhua@126.com>
2018-12-24 15:17:40 +0800

    add accounts configurations. to improve this future with json

---
+++ garagequant/dataservice/accountinfo_sample.py
@@ -0,0 +1,15 @@
+
+
+#todo use jason to do configurations
+
+garage_accounts = {
+    "Oanda": {
+        "id": "",
+        "token": ""
+    },
+
+    "Lmax": {
+        "id": "",
+        "password":""
+    }
+}
